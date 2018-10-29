e383c17f2212d800d4b5aafe0ebe8c0a1f970ee8
blablablub100 <clemenstisch@hotmail.de>
2018-10-29 20:17:12 +0100

    Added Picture Support

--- app/src/main/java/com/example/blablablub100/gemeinsameerinnerungen/MemoryAdapter.java
+++ app/src/main/java/com/example/blablablub100/gemeinsameerinnerungen/MemoryAdapter.java
@@ -1,21 +1,76 @@
 package com.example.blablablub100.gemeinsameerinnerungen;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.blablablub100.gemeinsameerinnerungen.ExperienceParser.Config;
 import com.example.blablablub100.gemeinsameerinnerungen.experienceLogic.Memory;
 import com.example.blablablub100.gemeinsameerinnerungen.util.ReadWriter;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MemoryAdapter extends RecyclerView.Adapter<MemoryAdapter.ViewHolder> {
+public class MemoryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    public class ViewHolderText extends RecyclerView.ViewHolder {
+
+        private TextView memoryNameTextView,memoryTextTextView;
+
+        public ViewHolderText(View itemView) {
+            super(itemView);
+            // Initiate view
+            memoryNameTextView = (TextView) itemView.findViewById(R.id.textView_memory_text_name);
+            memoryTextTextView = (TextView) itemView.findViewById(R.id.textView_memory_text_text);
+        }
+
+
+        public void showMemoryDetails(Memory memory){
+            // Attach values for each item
+            String memoryName = memory.getName();
+            String memoryText = "";
+            if (memory.getFile().getName().endsWith(".txt")) {
+                memoryText = ReadWriter.readFile(memory.getFile().getAbsolutePath());
+            } else {
+                memoryText = "noText";
+            }
+            memoryNameTextView.setText(memoryName);
+            memoryTextTextView.setText(memoryText);
+        }
+    }
+
+    public class ViewHolderPic extends RecyclerView.ViewHolder {
+
+        private TextView memoryNameTextView, memoryDescTextView;
+        private ImageView memoryPicImageView;
+
+        public ViewHolderPic(View itemView) {
+            super(itemView);
+            memoryNameTextView = (TextView) itemView.findViewById(R.id.textView_memory_pic_name);
+            memoryDescTextView = (TextView) itemView.findViewById(R.id.textView_memory_pic_decription);
+            memoryPicImageView = (ImageView) itemView.findViewById(R.id.imageView_memory_pic_pic);
+        }
+
+        public void showMemoryDetails(Memory memory) {
+            String memoryName = memory.getName();
+            String memoryDescription = memory.getDescription();
+            memoryNameTextView.setText(memoryName);
+            memoryDescTextView.setText(memoryDescription);
+            Picasso.with(context).load(memory.getFile()).centerCrop().resize(200, 300).into(memoryPicImageView);
+            System.out.println();
+        }
+
+    }
+
 
     private List<Memory> memories = new ArrayList<Memory>();
     private Context context;
@@ -30,18 +85,42 @@ public class MemoryAdapter extends RecyclerView.Adapter<MemoryAdapter.ViewHolder
 
     @NonNull
     @Override
-    public MemoryAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
-        int layout = R.layout.memory_text_layout;
-        View v = LayoutInflater
-                .from(parent.getContext())
-                .inflate(layout, parent, false);
-        return new ViewHolder(v);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        int layout;
+        View v;
+        switch (viewType) {
+            case 0:
+                layout = R.layout.memory_text_layout;
+                v = LayoutInflater
+                        .from(parent.getContext())
+                        .inflate(layout, parent, false);
+                return new ViewHolderText(v);
+            case 1:
+                layout = R.layout.memory_pic_layout;
+                v = LayoutInflater
+                        .from(parent.getContext())
+                        .inflate(layout, parent, false);
+                return new ViewHolderPic(v);
+        }
+        return null;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MemoryAdapter.ViewHolder holder, int position) {
+    public int getItemViewType(int position) {
+        return memories.get(position).getType();
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
         Memory memory = memories.get(position);
-        holder.showMemoryDetails(memory);
+        switch (holder.getItemViewType()) {
+            case 0:
+                ((ViewHolderText) holder).showMemoryDetails(memory);
+                break;
+            case 1:
+                ((ViewHolderPic) holder).showMemoryDetails(memory);
+                break;
+        }
     }
 
     @Override
@@ -52,34 +131,7 @@ public class MemoryAdapter extends RecyclerView.Adapter<MemoryAdapter.ViewHolder
 
 
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
 
-        private TextView memoryNameTextView,memoryTextTextView,memoryDateTextView;
-
-        public ViewHolder(View itemView) {
-            super(itemView);
-            // Initiate view
-            memoryNameTextView = (TextView) itemView.findViewById(R.id.textView_memory_text_name);
-            memoryDateTextView = (TextView) itemView.findViewById(R.id.textView_memory_text_date);
-            memoryTextTextView = (TextView) itemView.findViewById(R.id.textView_memory_text_text);
-        }
-
-
-        public void showMemoryDetails(Memory memory){
-            // Attach values for each item
-            String memoryDate =  Config.printdf.format(memory.getDate());
-            String memoryName = memory.getName();
-            String memoryText = "";
-            if (memory.getFile().getName().endsWith(".txt")) {
-                memoryText = ReadWriter.readFile(memory.getFile().getAbsolutePath());
-            } else {
-                memoryText = "noText";
-            }
-            memoryNameTextView.setText(memoryName);
-            memoryDateTextView.setText(memoryDate);
-            memoryTextTextView.setText(memoryText);
-        }
-    }
 
 
 }
--- app/src/main/java/com/example/blablablub100/gemeinsameerinnerungen/experienceLogic/Memory.java
+++ app/src/main/java/com/example/blablablub100/gemeinsameerinnerungen/experienceLogic/Memory.java
@@ -11,12 +11,37 @@ public class Memory implements Comparable<Memory> {
     private Date timestamp;
     private File content; //can be sound, vid, text, pic
     private String description;
+    private int fileType; // 0-TEXT, 1-PIC, 2-VID, 3-AUDIO
 
     public Memory(String name, Date timestamp, File content, String description) {
         this.name = name;
         this.timestamp = timestamp;
         this.description = description;
         this.content = content;
+        this.fileType = calcFileType(content);
+    }
+
+    private int calcFileType(File file) {
+        String ending = file.getAbsolutePath()
+                .substring(file.getAbsolutePath()
+                        .lastIndexOf(".")+1);
+        final String[] audFileExtensions =  new String[] {"mp3"};
+        final String[] picFileExtensions =  new String[] {"jpg", "png", "gif","jpeg", "gif"};
+        final String[] vidFileExtensions =  new String[] {"3pg", "mp4", "mkv","webm"};
+        final String[] txtFileExtensions =  new String[] {"txt"};
+        for (String ex: picFileExtensions) {
+            if (ending.equals(ex)) return 1;
+        }
+        for (String ex: txtFileExtensions) {
+            if (ending.equals(ex)) return 0;
+        }
+        for (String ex: vidFileExtensions) {
+            if (ending.equals(ex)) return 2;
+        }
+        for (String ex: audFileExtensions) {
+            if (ending.equals(ex)) return 3;
+        }
+        return 4;
     }
 
     @Override
@@ -41,5 +66,9 @@ public class Memory implements Comparable<Memory> {
 
     public String getDescription() {
         return description;
+    }
+
+    public int getType() {
+        return fileType;
     }
 }
